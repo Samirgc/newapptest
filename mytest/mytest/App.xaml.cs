86c441f26dd9c7ac1cf2020753ef9981a8a1cfdc
xamarin guy <xamaringuy111@outlook.com>
2018-08-01 15:58:20 +0530

    this is a test

---
+++ mytest/mytest/App.xaml.cs
@@ -0,0 +1,35 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace mytest
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            AppCenter.Start("android=f543a3bb-e79c-4ddd-8129-64035072591c;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
